f7e26f191909289741d71af12b79c4e7a18f517e
Blinston S. Fernandes <b2ornot2b@gmail.com>
2020-05-09 18:33:28 +0530

    mustang usb events parser with protobufs

---
+++ mustang.go
@@ -0,0 +1,204 @@
+package mustang
+
+import "fmt"
+import log "github.com/Sirupsen/logrus"
+
+/*import "sort"
+import "bytes"
+import "errors"
+
+import "encoding/binary"
+import "google.golang.org/protobuf/reflect/protoreflect"
+*/
+import "github.com/google/gousb"
+import "google.golang.org/protobuf/encoding/protojson"
+import "google.golang.org/protobuf/reflect/protoreflect"
+
+const ModelPosition = 16
+
+type Mustang struct {
+	epIn      *gousb.InEndpoint
+	epOut     *gousb.OutEndpoint
+	jsChannel []chan string
+	Channel   []chan string
+}
+
+func (m *Mustang) GetJsonChannels() (rx chan string, tx chan string) {
+	if m.jsChannel == nil {
+		m.jsChannel = make([]chan string, 2)
+		m.jsChannel[0] = make(chan string)
+		m.jsChannel[1] = make(chan string)
+
+		go m.senderLoop(m.jsChannel[1])
+	}
+	return m.jsChannel[0], m.jsChannel[1]
+}
+
+func (m *Mustang) Connect() {
+	m.usbConnect()
+}
+
+func (m *Mustang) senderLoop(tx chan string) {
+	for {
+		msg := <-tx
+		log.Warn("Received: ", msg)
+		m := &UpdateAmp{}
+		err := protojson.Unmarshal([]byte(msg), m)
+		if err != nil {
+			log.Error("Invalid JSON", err)
+			continue
+		}
+
+	}
+}
+
+func (m *Mustang) usbReaderLoop() {
+	buf := make([]byte, m.epIn.Desc.MaxPacketSize)
+
+	stream, err := m.epIn.NewStream(m.epIn.Desc.MaxPacketSize, 1)
+	if err != nil {
+		log.Fatal("NewStream failed", err)
+	}
+
+	for {
+		readBytes, err := stream.Read(buf)
+		if err != nil {
+			log.Error("stream read failed", err)
+			return
+		}
+
+		p := MustangUsbProtocolParser{}
+		// m.displayBuf(buf, readBytes)
+		msg, err := p.parse(buf)
+		if err != nil {
+			log.Warn("Unhandled message...")
+			m.displayBuf(buf, readBytes)
+			continue
+		}
+
+		if msg == nil {
+			log.Debug("Silently dropping message...")
+			continue
+		}
+
+		r := msg.ProtoReflect()
+		m.toOsc(r, "/")
+
+		if m.jsChannel != nil {
+			marshal := protojson.MarshalOptions{EmitUnpopulated: true}
+			json, err := marshal.Marshal(msg)
+			if err != nil {
+				continue
+			}
+			jsonStr := string(json[:])
+			// log.Printf("msg %T: %s\n", msg, jsonStr)
+			m.jsChannel[0] <- jsonStr
+		}
+	}
+}
+
+func (m *Mustang) toOsc(r protoreflect.Message, keyPrefix string) {
+	log.Info("toOsc")
+	r.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+		path := keyPrefix + fd.JSONName()
+		switch fd.Kind() {
+		case protoreflect.StringKind:
+			fmt.Printf("Key=%v Value=%v\n", path, v.String())
+		case protoreflect.BoolKind:
+			fmt.Printf("Key=%v Value=%v\n", path, v.Bool())
+		case protoreflect.Uint32Kind:
+			fmt.Printf("Key=%v Value=%v\n", path, v.Uint())
+		case protoreflect.MessageKind:
+			if fd.IsList() {
+				l := v.List()
+				for i := 0; i < l.Len(); i++ {
+					m.toOsc(l.Get(i).Message(), path+"/")
+					//m.toOsc(v.List())
+				}
+			}
+
+		default:
+			fmt.Printf("Key=%v Kind=%d Values=LIST", fd.JSONName(), fd.Kind())
+		}
+		return true
+	})
+}
+func (m *Mustang) displayBuf(buf []byte, readBytes int) {
+	for i := 0; i < readBytes; i++ {
+		fmt.Printf("\t%d: %02x %q", i, buf[i], buf[i])
+		if 0 == ((i + 1) % 8) {
+			fmt.Println()
+		}
+	}
+}
+
+func (m *Mustang) usbInit() {
+	buf := make([]byte, m.epOut.Desc.MaxPacketSize)
+	buf[1] = 0xc3
+	m.usbSend(buf)
+
+	buf[0] = 0x1a
+	buf[1] = 0xc1
+	m.usbSend(buf)
+}
+
+func (m *Mustang) usbSend(buf []byte) {
+	epOut := m.epOut
+	for retries := 0; retries < 64; retries++ {
+		writeBytes, err := epOut.Write(buf)
+		if err != nil {
+			fmt.Println("Write returned an error:", err)
+			continue
+		}
+
+		log.Debug("wrote bytes", writeBytes)
+		break
+	}
+}
+
+func (m *Mustang) usbConnect() {
+	ctx := gousb.NewContext()
+	defer ctx.Close()
+	vid, pid := gousb.ID(0x1ed8), gousb.ID(0x0016)
+	devs, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
+		return desc.Vendor == vid && desc.Product == pid
+	})
+	for _, d := range devs {
+		defer d.Close()
+	}
+	if err != nil {
+		log.Fatalf("OpenDevices(): %v", err)
+	}
+	if len(devs) == 0 {
+		log.Fatalf("no devices found matching VID %s and PID %s", vid, pid)
+	}
+
+	dev := devs[0]
+
+	err = dev.SetAutoDetach(true)
+	if err != nil {
+		log.Fatalf("setting auto detach failed %v", err)
+	}
+
+	intf, done, err := dev.DefaultInterface()
+	if err != nil {
+		log.Fatalf("%s.DefaultInterface(): %v", dev, err)
+	}
+	defer done()
+	defer intf.Close()
+
+	epIn, err := intf.InEndpoint(1)
+	if err != nil {
+		log.Fatalf("%s.InEndpoint(1): %v", intf, err)
+	}
+	m.epIn = epIn
+
+	epOut, err := intf.OutEndpoint(1)
+	if err != nil {
+		log.Fatalf("%s.OutEndpoint(1: %v", intf, err)
+	}
+	m.epOut = epOut
+
+	m.usbInit()
+	m.usbReaderLoop()
+}
---
+++ usbprotocol.go
@@ -0,0 +1,123 @@
+package mustang
+
+//import "fmt"
+//import "log"
+import "sort"
+import "bytes"
+import "errors"
+
+import "encoding/binary"
+import "google.golang.org/protobuf/reflect/protoreflect"
+
+//go:generate protoc --go_out=. mustangusb.proto
+
+type MustangUsbProtocolParser struct {
+	buf []byte
+}
+
+type Message interface {
+	ProtoReflect() protoreflect.Message
+}
+
+type paramNamesMap map[uint16](map[uint8]string)
+
+func (p *MustangUsbProtocolParser) parseEffect(
+	buf []byte,
+	effect string,
+	ids map[uint16]string,
+	paramNames paramNamesMap) (*EffectChange, error) {
+	model := binary.LittleEndian.Uint16(buf[16:18])
+	name := ids[model]
+	enabled := false
+	if buf[22] == 0 {
+		enabled = true
+	}
+	params := p.parseEffectParams(paramNames[model])
+	m := EffectChange{
+		Effect:  effect,
+		Name:    name,
+		Model:   uint32(model),
+		Enabled: enabled,
+		Params:  params,
+	}
+	return &m, nil
+}
+
+func (p *MustangUsbProtocolParser) parseEffectParams(paramNames map[uint8]string) []*Parameter {
+	params := make([]*Parameter, 0, len(paramNames))
+	for idx, paramName := range paramNames {
+		value := p.buf[32+idx]
+		params = append(params, &Parameter{
+			Name:    paramName,
+			Control: uint32(idx),
+			Value:   uint32(value),
+		})
+	}
+	sort.Slice(params, func(i, j int) bool {
+		return params[i].Control < params[j].Control
+	})
+	return params
+}
+
+func (p *MustangUsbProtocolParser) parse(buf []byte) (Message, error) {
+	p.buf = buf
+	switch binary.BigEndian.Uint16(buf[0:2]) {
+	case 0x0100:
+		lastIdx := bytes.IndexByte(buf[4:], byte(0))
+		version := string(buf[4 : 4+lastIdx])
+		m := Version{Version: version}
+		return &m, nil
+	case 0x0501:
+		dsp := uint32(buf[2])
+		effect := uint32(buf[3])
+		control := uint32(buf[5])
+		value := uint32(binary.BigEndian.Uint16(buf[9:11]))
+		if control >= 15 && control <= 20 {
+			value >>= 8
+		}
+		m := ParameterChange{Dsp: dsp,
+			Effect:  effect,
+			Control: control,
+			Value:   value,
+		}
+		return &m, nil
+	case 0x1c01:
+		dsp := uint32(buf[2])
+		category := uint32(buf[3])
+		idx := uint32(buf[4])
+		lastIdx := bytes.IndexByte(buf[16:], byte(0))
+		name := string(buf[16 : 16+lastIdx])
+		switch dsp {
+		case 0x04:
+			m := PatchnameChange{
+				Category: category,
+				Idx:      idx,
+				Name:     name,
+			}
+			return &m, nil
+		case 0x05:
+			model := binary.LittleEndian.Uint16(buf[16:18])
+			name := ampIds[model]
+			params := p.parseEffectParams(ampParamNames)
+			m := EffectChange{
+				Effect: "Amp",
+				Name:   name,
+				Model:  uint32(model),
+				Params: params,
+			}
+			return &m, nil
+		case 0x06:
+			return p.parseEffect(buf, "Stompbox", stompboxIds, stompboxParamNames)
+		case 0x07:
+			return p.parseEffect(buf, "Modulation", modulationIds, modulationParamNames)
+		case 0x08:
+			return p.parseEffect(buf, "Delay", delayIds, delayParamNames)
+		case 0x09:
+			return p.parseEffect(buf, "Reverb", reverbIds, reverbParamNames)
+		default:
+			return nil, nil
+		}
+
+	}
+	return nil, errors.New("Unknown message type")
+}
